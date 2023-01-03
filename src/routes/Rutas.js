9ebd6258789b677e9d8085d40bfc7bbec7cf3fd1
Javier Herzkovich <javierdash@gmail.com>
2023-01-03 09:33:46 -0300

    probando el navbar con bootstrap

--- src/routes/Rutas.js
+++ src/routes/Rutas.js
@@ -1,12 +1,15 @@
 import {Route, Routes, BrowserRouter as Router} from "react-router-dom"
+//components
 import Home from "../components/Home"
 import Buscador from "../components/Buscador"
 import Resultados from "../components/Resultados"
+import Navbar from "../components/Navbar"
 
 const Rutas = () => {
   return (
     <>
     <Router>
+      <Navbar />
       <Routes>
             <Route path="/" exact element=<Home /> />
             <Route path="/buscador" exact element=<Buscador /> />
