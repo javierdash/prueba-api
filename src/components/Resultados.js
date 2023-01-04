db9b166d859474dbd3545879eaeb69649d7dff8f
Javier Herzkovich <javierdash@gmail.com>
2023-01-04 09:29:08 -0300

    comparando Card vs div estilizado

--- src/components/Resultados.js
+++ src/components/Resultados.js
@@ -7,11 +7,12 @@ const Resultados = () => {
 
   const palabraABuscar = "juguete"
   useEffect(() => {
-    const endpoint = `https://api.mercadolibre.com/sites/MLA/search?q=${palabraABuscar}&limit=1`
+    const endpoint = `https://api.mercadolibre.com/sites/MLA/search?q=${palabraABuscar}&limit=6`
     axios.
           get(endpoint)
           .then(res => {
-            const data = res.data.results[0]
+            // const data = res.data.results[0]
+            const data = res.data.results
             console.log(data)
             setResultado(data)
           })
@@ -21,15 +22,29 @@ const Resultados = () => {
   return (
     <>
     {resultado === undefined && <h4>No se encontraron resultados</h4>}
-    {resultado &&
-      <>
-      <h3>{resultado.title}</h3>
-      <img src={resultado.thumbnail} />
-      {/* <h4>Precio: ${resultado.installments.amount}</h4> */}
-      </>
+    {resultado.map(cadaUno => {
+      return (
+        <>
+        <div style={{border: "1px solid grey", margin: "3px"}}>
+        <h3>{cadaUno.title}</h3>
+        <img src={cadaUno.thumbnail} />
+        </div>
+        </>
+      )
+    })
     }
     </>
   )
 }
 
 export default Resultados
+
+
+// {resultado &&
+//   <>
+//   <div style={{border: "1px solid grey"}}>
+//   <h3>{resultado.title}</h3>
+//   <img src={resultado.thumbnail} />
+//   <h4>Precio: ${resultado.installments.amount}</h4>
+//   </div>
+//   </>
